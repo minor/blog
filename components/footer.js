2e66c6420f52fb53eefe510aa201e1477fc00282
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2020-07-24 16:50:09 -0700

    [update] footer.js (1)

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h4 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <h4 className="text-2xl lg:text-5xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             © Copyright 2020 - Saurish Srivastava
           </h4>
         </div>
