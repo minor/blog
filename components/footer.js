613f8624e2673e34c5ac57e51822c636fc96c0be
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2020-08-26 12:15:22 -0700

    [update] footer.js - cleaner

--- components/footer.js
+++ components/footer.js
@@ -7,35 +7,32 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <section id={utilStyles.navbar} className={utilStyles.listinline}>
-          <ul className={utilStyles.listinline}>
-            <div className={utilStyles.navbarref}>
-              <a href="https://saurish.com/">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-              <a href="https://saurish.com/about">About</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-              <a href="mailto:me@saurish.com?subject=Hello from the Website!">Contact</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-              <a href="https://saurish.com/projects">Projects</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-              <Link href="/"><a>Blog</a></Link>
-            </div>
-            <br />
-          </ul>
-        </section>
-        <section className={utilStyles.social}>
-          <a target="_blank" href="https://www.facebook.com/saurish.srivastava.10/">
-            <img className={utilStyles.titlelogo} src="/assets/images/facebook.svg" />
-          </a>
-          <a target="_blank" href="https://twitter.com/saurishhh">
-            <img className={utilStyles.titlelogo} src="/assets/images/twitter.svg/" />
-          </a>
-          <a target="_blank" href="https://www.instagram.com/saurishhh/">
-            <img className={utilStyles.titlelogo} src="/assets/images/insta.svg" />
-          </a>
-          <a target="_blank" href="https://github.com/saurishs/">
-            <img className={utilStyles.titlelogo} src="/assets/images/github.svg" />
-          </a>
+        <section className={utilStyles.headingNavigation}>
+          <div className={utilStyles.navigation}>
+            <Link href="/"><a>Home</a></Link>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link href="/about"><a>About</a></Link>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="mailto:me@saurish.com?subject=Hello from the Website!">Contact</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link href="/projects"><a>Projects</a></Link>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="https://blog.saurish.com/">Blog</a>
+          </div>
         </section>
         <br />
-        <section className={utilStyles.footercredits}>
-          &copy; Copyright 2020 - Saurish Srivastava
+        <section className={utilStyles.social}>
+          <a target="_blank" href="https://www.facebook.com/saurish.srivastava.10/">
+            <img className={utilStyles.titlelogo} src="/images/facebook.svg"></img>
+          </a>
+          <a target="_blank" href="https://twitter.com/saurishhh">
+            <img className={utilStyles.titlelogo} src="/images/twitter.svg/"></img>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/saurishhh/">
+            <img className={utilStyles.titlelogo} src="/images/insta.svg"></img>
+          </a>
+          <a target="_blank" href="https://github.com/saurishs/">
+            <img className={utilStyles.titlelogo} src="/images/github.svg"></img>
+          </a>
+        </section>
+        <section className={utilStyles.headingFooter}>
+          <p><center>&copy; Copyright 2020 - Saurish Srivastava</center></p>
         </section>
       </Container>
     </footer>
