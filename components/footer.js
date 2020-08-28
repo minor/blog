47e684fcb8ee296b1ba2580816f8903b143dc623
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2020-08-27 20:38:34 -0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -7,6 +7,7 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
+        <br />
         <section className={utilStyles.headingNavigation}>
           <div className={utilStyles.navigation}>
             <Link href="/"><a>Home</a></Link>&nbsp;&nbsp;|&nbsp;&nbsp;
